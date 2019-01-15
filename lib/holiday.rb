55e9355030cc739f87183e65b41d194e985bf822
Samidh Patel <spatel13456@gmail.com>
2019-01-15 21:57:44 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,6 +66,8 @@ def all_supplies_in_holidays(holiday_hash)
       holiday = holiday.to_s.split("_").map 
         {|w| w.capitalize}.join(" ") << ":"
       puts "  " + holiday + " " + supplies
+    end
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
