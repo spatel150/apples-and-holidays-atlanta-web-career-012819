4977c3876bbb8d4f204ffcd288ad9f39927c2a4a
Samidh Patel <spatel13456@gmail.com>
2019-01-15 20:40:13 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,6 +18,10 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  holiday_hash.each do |seasons, data|
+    data.each do |attribute, value|
+      if att
+  
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
