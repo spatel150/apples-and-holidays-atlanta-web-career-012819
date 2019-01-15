6b1a5856bad2080743e99021defb827d410e249a
Samidh Patel <spatel13456@gmail.com>
2019-01-15 20:41:30 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -21,6 +21,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
   holiday_hash.each do |seasons, data|
     data.each do |attribute, value|
       if attribute == :summer 
+        value.each do 
   
 end
 
